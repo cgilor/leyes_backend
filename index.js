0aa38f12ab86c5efe4f97e78c5f7f013bd1ea804
Carlos Gil <cgil.or@gmail.com>
2020-10-14 18:54:11 -0600

    backend 2.0.0

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
+
 const cors =require('cors');
 const { dbConnection } = require('./database/config');
 
@@ -23,7 +25,10 @@ app.use('/api/todo', require ('./routes/busquedas'));
 app.use('/api/login', require ('./routes/auth'));
 app.use( '/api/upload', require('./routes/uploads') );
 
-
+// Lo último
+app.get('*', (req, res) => {
+     res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+ });
 
 app.listen( process.env.PORT, () => {
      console.log('Servidor running' + process.env.PORT);
